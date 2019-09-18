ef4ed930fec165f1eced01e192d0cac30ed0d485
流氓凡 <wangyifani@foxmail.com>
2019-09-18 11:44:03 +0800

    1.31
    修复正则

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ error_reporting(0); //关闭错误报告
 $seach=file_get_contents('https://www.360kan.com/');
 $szz="# <a href='(.*?)' class='p0 g-playicon js-playicon' ><img src='(.*?)' alt='(.*?)' /><span class='title'>(.*?)</span><span class='desc'>(.*?)</span><b></b>#";
 preg_match_all($szz,$seach,$sarr);
-$one=$sarr[1];//标题
+$one=$sarr[1];
 $two=$sarr[2];
 $three=$sarr[5];
 include 'data/dyjx.php';
